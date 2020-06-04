75af47803ff2404bcb2841b959a8325386cbce43
Larry Bradley <larry.bradley@gmail.com>
2020-06-03 20:27:32 -0400

    Update platform tags for mac os

--- autowheel/config.py
+++ autowheel/config.py
@@ -2,7 +2,7 @@ PYTHON_TAGS = ['cp27', 'cp34', 'cp35', 'cp36', 'cp37', 'cp38', 'cp39']
 
 # The platform tags use syntax compatible with fnmatch since that is what is
 # used by cibuildwheel, and also when we check existing builds on PyPI.
-PLATFORM_TAGS = {'macosx': 'macosx_10_?_*',
+PLATFORM_TAGS = {'macosx': 'macosx_*',
                  'linux32': 'manylinux*_i686',
                  'linux64': 'manylinux*_x86_64',
                  'windows32': 'win32',
